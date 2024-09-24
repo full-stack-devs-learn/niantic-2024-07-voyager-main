adb3e7b24dcb82caf936ece87ae159ed225f0a74
Gregor Dzierzon <gdzierzon@gmail.com>
2024-09-24 13:27:52 -0600

    demo and prep

---
+++ sprint-3/10-introduction-to-react/demo-create-final/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootswatch/dist/united/bootstrap.min.css'
+
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
