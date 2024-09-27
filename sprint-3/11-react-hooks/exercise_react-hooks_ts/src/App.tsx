270a52d54fe95eecb6e01e7759314705f299ff94
Gregor Dzierzon <gdzierzon@gmail.com>
2024-09-27 14:02:30 -0600

    demos

---
+++ sprint-3/11-react-hooks/exercise_react-hooks_ts/src/App.tsx
@@ -0,0 +1,28 @@
+import { SetStateAction, useState } from "react"
+import Header from "./components/header/Header"
+import CategoriesPage from "./components/categories/cagtegories-page/CategoriesPage"
+import ProductsPage from "./components/products/products-page/ProductsPage"
+
+function App() {
+  const [pageName, setPageName] = useState("categories")
+
+  const pageChangeHanlder = (newPage: SetStateAction<string>) =>
+  {
+    setPageName(newPage);
+    console.log("changing page to: " + pageName); 
+  }
+
+  console.log("page render: " + pageName)
+  
+
+  return (
+    <>
+      <Header onPageChanged={pageChangeHanlder}></Header>
+
+      { (pageName=="categories") ? <CategoriesPage></CategoriesPage> : null}
+      { (pageName=="products") && <ProductsPage></ProductsPage> }
+    </>
+  )
+}
+
+export default App
