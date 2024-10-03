e1c97b9191f09f12333e1950754bd764dd679dca
Gregor Dzierzon <gdzierzon@gmail.com>
2024-10-03 10:08:09 -0600

    removed button from home and unnecessary imports

--- sprint-3/16-api-authentication/capstone-starter-react/src/components/home/Home.tsx
+++ sprint-3/16-api-authentication/capstone-starter-react/src/components/home/Home.tsx
@@ -1,17 +1,6 @@
-import { useState } from "react";
-import demoService from "../../services/demo-service"
 
 export default function Home()
 {
-    const [data, setData] = useState<any>('');
-
-    async function simpleClickHandler()
-    {
-        const result = await demoService.getDemo()
-        console.log(result);
-        setData(result)
-        
-    }
 
     return (
         <>
@@ -19,11 +8,6 @@ export default function Home()
             Home
         </h1>
 
-        <button onClick={simpleClickHandler}>Simple Authenticated Demo</button>
-
-        <div>
-            {data}
-        </div>
 
         </>
     )
